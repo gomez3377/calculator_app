67f422693cdaa9b4e928ad5c4839605d42419919
Jonathon Gomez <jongomez3377@gmail.com>
2022-11-16 21:45:35 -0700

    Wednesday Night Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import React from 'react'
 import Buttons from './components/Buttons'
+import NumOne from './components/NumOne'
+import NumTwo from './components/NumTwo'
 import './index.css'
-
+import {nanoid} from 'nanoid'
 
 function App() {
-  const [buttonFaces,setbuttonFaces] = React.useState([
+  const [firstNumberValue, setFirstNumberValue] = React.useState("")
+  const [secondNumberValue, setSecondNumberValue] = React.useState("")
+  
+
+  const buttons ={ 
+    buttonFaces:[
     "AC","+/-", "%", "/", 
     7, 8, 9, "*",
     4,5,6, "-",
     1,2,3, "+",
-    0,".","=" 
-  ])
+    0,".","=" ],
+    id:nanoid()
+  }
 
   
+  function numButtonClicked(){
 
-  const buttonElements = buttonFaces.map(buttonValue =><Buttons buttonValue={buttonValue}/> )
+    console.log("button clicked!!")
+
+  }
+  
+
+  const buttonElements = buttons.buttonFaces.map(buttonValue =><Buttons 
+    key={buttons.id}
+    id={buttons.id}
+    buttonClicked={numButtonClicked} 
+    buttonValue={buttonValue}/> )
   
 
 
 
 
   return (
-    <main className="App flex items-center bg-lightGreenishGray h-screen w-screen">
-      <div id='calculator-container' className='w-1/2 h-1/2 mx-auto bg-gray-900/50'>
-        <div className="operation-container w-full bg-pink-100 h-1/4">
-           <h1>123</h1>
-           <h1>456</h1>
+    <main className="App flex items-center h-screen w-screen">
+      <div id='calculator-container' className='w-1/2 h-1/2 mx-auto border border-white'>
+        <div className="operation-container relative flex flex-col w-full bg-black/25 h-1/4 text-white">
+            <NumOne />
+            <NumTwo value={secondNumberValue} />
         </div>
         
         <div className="calcualtor-buttons grid grid-cols-4 grid-row-3 gap-0 w-full h-3/4">
