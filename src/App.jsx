b493a8268a1dc66eb99addd05400e95701bc991a
gomez3377 <jongomez3377@gmail.com>
2022-11-17 03:24:16 -0700

    thursday changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,128 @@
-import React from 'react'
-import Buttons from './components/Buttons'
-import NumOne from './components/NumOne'
-import NumTwo from './components/NumTwo'
-import './index.css'
-import {nanoid} from 'nanoid'
+import React from "react";
+import Buttons from "./components/Buttons";
+import NumOne from "./components/NumOne";
+import NumTwo from "./components/NumTwo";
+import "./index.css";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [firstNumberValue, setFirstNumberValue] = React.useState("")
-  const [secondNumberValue, setSecondNumberValue] = React.useState("")
-  
+  const [firstNumberValue, setFirstNumberValue] = React.useState("");
+  const [secondNumberValue, setSecondNumberValue] = React.useState("");
 
-  const buttons ={ 
-    buttonFaces:[
-    "AC","+/-", "%", "/", 
-    7, 8, 9, "*",
-    4,5,6, "-",
-    1,2,3, "+",
-    0,".","=" ],
-    id:nanoid()
+  const [buttons, setButtons] = React.useState(generateButtons());
+
+  function generateButtons() {
+    const buttonValues = [
+      "AC",
+      "+/-",
+      "%",
+      "/",
+      7,
+      8,
+      9,
+      "*",
+      4,
+      5,
+      6,
+      "-",
+      1,
+      2,
+      3,
+      "+",
+      0,
+      ".",
+      "=",
+    ];
+    const buttonsArray = [];
+    for (let i = 0; i < 19; i++) {
+      buttonsArray.push({
+        buttonValue: buttonValues[i],
+        id: nanoid(),
+      });
+    }
+    return buttonsArray;
   }
+React.useEffect(()=> {
+
+},[])
 
   
-  function numButtonClicked(){
 
-    console.log("button clicked!!")
+  function buttonClicked(currendId) {
+    let currentOperator
+    let currentNumber
+    let previousNumber
+    let currentButton;
 
+    buttons.find((button) => {
+      if (button.id === currendId) {
+        currentButton = button.buttonValue;
+          if(typeof currentButton === "number"){
+          setFirstNumberValue(prevNum => prevNum + currentButton)
+
+        }else if(currentButton === "+") {
+          console.log("You added!")
+          
+        }
+        
+        else if(currentButton === "AC"){
+          setSecondNumberValue("")
+        }
+
+      }
+      
+      return currentButton
+    });
+
+    
+  }
+  console.log(secondNumberValue)
+  function addNumbers(x,y) {
+    return x + y
+  }
+  function subtractNumbers(x,y) {
+    return x - y
+  }
+  function multiplyNumbers(x,y) {
+    return x * y
+  }
+  function divideNumbers(x,y) {
+    return x / y
   }
   
-
-  const buttonElements = buttons.buttonFaces.map(buttonValue =><Buttons 
-    key={buttons.id}
-    id={buttons.id}
-    buttonClicked={numButtonClicked} 
-    buttonValue={buttonValue}/> )
+  
   
 
 
 
+
+
+  const buttonElements = buttons.map((button) => (
+    <Buttons
+      key={button.id}
+      id={button.id}
+      buttonClicked={() => buttonClicked(button.id)}
+      buttonValue={button.buttonValue}
+    />
+  ));
 
   return (
     <main className="App flex items-center h-screen w-screen">
-      <div id='calculator-container' className='w-1/2 h-1/2 mx-auto border border-white'>
+      <div
+        id="calculator-container"
+        className="w-1/2 h-1/2 mx-auto border border-white"
+      >
         <div className="operation-container relative flex flex-col w-full bg-black/25 h-1/4 text-white">
-            <NumOne />
-            <NumTwo value={secondNumberValue} />
+          <NumOne value={firstNumberValue} />
+          <NumTwo value={secondNumberValue} />
         </div>
-        
-        <div className="calcualtor-buttons grid grid-cols-4 grid-row-3 gap-0 w-full h-3/4">
-         
-         {buttonElements}
 
-     
+        <div className="calcualtor-buttons grid grid-cols-4 grid-row-3 gap-0 w-full h-3/4">
+          {buttonElements}
         </div>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NumOne.jsx
+++ src/components/NumOne.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function NumOne(){
+export default function NumOne(props){
     return(
-        <h1 className='px-4 py-2 absolute top-0 right-0 text-2xl'></h1>
+        <h1 className='p-4 absolute bottom-0 right-0 text-5xl'>{props.value}</h1>
     )
 }
